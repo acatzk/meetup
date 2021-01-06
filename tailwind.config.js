8e45cfc14971a2265ecce08d2f7256fbbee71f15
Joshua Galit <joshuaimalay@gmail.com>
2021-01-06 09:15:27 +0800

    add: meetup-blue modified color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'meetup-blue': '#00455D'
+      }
+    },
   },
   variants: {},
   plugins: [],
