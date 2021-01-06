349b0a5029bb9c2db29dfbb1deed4c4daac58fd8
Joshua Galit <joshuaimalay@gmail.com>
2021-01-06 16:38:08 +0800

    plugins: registered airbnb-style-datepicker in plugin folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,10 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    {
+      src: '~/plugins/vue-airbnb-style-datepicker.js',
+      ssr: false
+    }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
