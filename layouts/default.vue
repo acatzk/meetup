f5f915d1cbffa4c3493b4eb85f2b2d84d6b9873e
Joshua Galit <joshuaimalay@gmail.com>
2021-01-06 09:01:49 +0800

    import: TheNavBar component nested: Nuxt component inside main html tag add: general font, background & antialized

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,16 @@
 <template>
-  <Nuxt />
+  <div class="font-sans bg-gray-200 h-screen antialiased">
+    <TheNavBar />
+    <main>
+      <Nuxt />
+    </main>
+  </div>
 </template>
+
+<script>
+  export default {
+    components: {
+      TheNavBar: () => import('@/components/TheNavBar')
+    }
+  }
+</script>
